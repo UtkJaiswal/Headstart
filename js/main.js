d7a7c03d29bffa55c0cb24061ba38d2980d047f4
Utkarsh Jaiswal <utkjaiswal58@gmail.com>
2020-04-20 00:37:37 +0530

    Contact us added

--- js/main.js
+++ js/main.js
@@ -213,7 +213,8 @@ function myFunction() {
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+
+window.onmouseout = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
